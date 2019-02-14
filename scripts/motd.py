560ac67213777bf226d79f339745701fefcfa421
dsavov <boo@ubuntu-server-1804-vm.5ivzuvscu3cevbauiriwsu5d0f.bx.internal.cloudapp.net>
2019-02-14 14:53:13 +0000

    initial commit

---
+++ scripts/motd.py
@@ -0,0 +1,17 @@
+import datetime
+import socket
+
+filepath = "/etc/motd"
+current_datetime = str(datetime.datetime.now())
+hash_current_datetime = str(hash(datetime.datetime.now()))
+hostname = socket.gethostname()    
+ip_addr = socket.gethostbyname(hostname) 
+
+try:
+    with open(filepath, "w+") as outfile:
+        outfile.write("current_datetime:" + current_datetime + "\n")
+        outfile.write("hash_current_datetime:" + hash_current_datetime + "\n")
+        outfile.write("hostname:" + hostname + "\n")
+        outfile.write("ip_address" + ip_addr + "\n")
+except IOError:
+    print("Couldn't open/create/write to file:" + filepath)
